35de029de0f07a35d3b0ef009487d50732944003
jihoonheo <modernflow@tripstore.kr>
2022-10-03 14:59:17 +0900

    init: First commit
    gradle
    - thymeleaf
    - spring security
    - spring data jpa

---
+++ src/main/java/com/example/all/App.java
@@ -0,0 +1,13 @@
+package com.example.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
