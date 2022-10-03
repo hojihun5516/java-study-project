474748d5db9e0a2477fbf9c53a6984b97b02688a
hojihun5516 <hojihun5516@daum.net>
2022-10-04 00:18:04 +0900

    feat: Add signUpFrom Validator

---
+++ src/main/java/com/example/all/account/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.example.all.account;
+
+import com.example.all.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByNickName(String nickname);
+
+    boolean existsByEmail(String email);
+}
