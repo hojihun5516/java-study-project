773f79451b446b321b6e23252391df4ddbb49582
jihoonheo <modernflow@tripstore.kr>
2022-10-03 23:41:16 +0900

    feat: Security config, signUp end-point
    - signup endpoint
    - template
    - spring security config

---
+++ src/main/java/com/example/all/account/SignUpForm.java
@@ -0,0 +1,14 @@
+package com.example.all.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+}
---
+++ src/main/java/com/example/all/domain/Account.java
@@ -0,0 +1,21 @@
+package com.example.all.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
