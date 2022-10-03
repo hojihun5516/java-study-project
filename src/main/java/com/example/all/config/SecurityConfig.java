3298462304eeb9346c221518515feecb05fdecc5
jihoonheo <modernflow@tripstore.kr>
2022-10-03 23:41:16 +0900

    feat: Security config, signUp end-point
    - signup endpoint
    - template
    - spring security config

---
+++ src/main/java/com/example/all/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.all.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.mvcMatchers("/", "/login", "/sign-up", "/check-email",
+                        "/check-email-token", "/email-lgoin", "/check-email-login", "/login-link", "profile/*").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated());
+        return http.build();
+
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
+
