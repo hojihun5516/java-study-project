52a2c8f286e4451b5f13bce5c91038d704c84b67
jihoonheo <modernflow@tripstore.kr>
2022-10-06 06:57:52 +0900

    feat: Encrypt password

--- src/main/java/com/example/all/account/AccountRepository.java
+++ src/main/java/com/example/all/account/AccountRepository.java
@@ -9,4 +9,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     boolean existsByNickname(String nickname);
 
     boolean existsByEmail(String email);
+
+    Account findByEmail(String email);
 }
