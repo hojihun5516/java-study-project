16d92f8caa3245e501fd3a1893de5b18f29a89ee
jihoonheo <modernflow@tripstore.kr>
2022-10-10 22:09:24 +0900

    test: login, logout test code

---
+++ src/test/java/com/example/all/main/MainControllerTest.java
@@ -0,0 +1,107 @@
+package com.example.all.main;
+
+import com.example.all.account.AccountRepository;
+import com.example.all.account.AccountService;
+import com.example.all.account.SignUpForm;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("이메일로 로그인 성공")
+    void login_with_email() throws Exception {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("jihoon");
+        signUpForm.setEmail("jihoon@gmail.com");
+        signUpForm.setPassword("12341234");
+        accountService.processNewAccount(signUpForm);
+
+        mockMvc.perform(post("/login")
+                        .param("username", "jihoon@gmail.com")
+                        .param("password", "12341234")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("jihoon"));
+    }
+
+    @Test
+    @DisplayName("닉네임으로 로그인 성공")
+    void login_with_nickname() throws Exception {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("jihoon");
+        signUpForm.setEmail("jihoon@gmail.com");
+        signUpForm.setPassword("12341234");
+        accountService.processNewAccount(signUpForm);
+
+        mockMvc.perform(post("/login")
+                        .param("username", "jihoon")
+                        .param("password", "12341234")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(authenticated().withUsername("jihoon"));
+    }
+
+
+    @Test
+    @DisplayName("로그인 실패")
+    void login_fail() throws Exception {
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname("jihoon");
+        signUpForm.setEmail("jihoon@gmail.com");
+        signUpForm.setPassword("12341234");
+        accountService.processNewAccount(signUpForm);
+
+        mockMvc.perform(post("/login")
+                        .param("username", "unknown")
+                        .param("password", "12341234")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("로그아웃")
+    void logout() throws Exception {
+        mockMvc.perform(post("/logout")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"))
+                .andExpect(unauthenticated());
+    }
+
+}
