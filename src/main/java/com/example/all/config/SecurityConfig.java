3e0c6d6ee4837d757417d49b42d155a043f1b79e
moflow <32387358+hojihun5516@users.noreply.github.com>
2022-10-11 06:48:12 +0900

    Revert "feat: Remember-me setting"
    This reverts commit f8f5b9a26a67be939f91dd9266a9e3c242aa1b46.

--- src/main/java/com/example/all/config/SecurityConfig.java
+++ src/main/java/com/example/all/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.example.all.config;
 
-import com.example.all.account.AccountService;
-import com.example.all.domain.PersistentLogins;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AccountService accountService;
-    private final DataSource dataSource;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz.mvcMatchers("/", "/login", "/sign-up",
@@ -34,19 +24,7 @@ public class SecurityConfig {
         http.formLogin().loginPage("/login").permitAll();
 
         http.logout().logoutSuccessUrl("/");
-
-        http.rememberMe()
-                .userDetailsService(accountService)
-                .tokenRepository(tokenRepository());
-
         return http.build();
-    }
-
-    @Bean
-    public PersistentTokenRepository tokenRepository() {
-        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
-        jdbcTokenRepository.setDataSource(dataSource);
-        return jdbcTokenRepository;
     }
 
     @Bean
