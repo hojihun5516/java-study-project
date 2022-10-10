a51e259fb9e1500e9f7f41a0c9dd90052d53dac2
jihoonheo <modernflow@tripstore.kr>
2022-10-10 23:24:44 +0900

    bug: Set transactional email confirm

--- src/main/java/com/example/all/account/AccountService.java
+++ src/main/java/com/example/all/account/AccountService.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AccountService implements UserDetailsService {
 
@@ -67,6 +68,7 @@ public class AccountService implements UserDetailsService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String emailOrNickname) throws UsernameNotFoundException {
         Account account = accountRepository.findByEmail(emailOrNickname);
         if (account==null){
@@ -76,5 +78,10 @@ public class AccountService implements UserDetailsService {
             throw new UsernameNotFoundException(emailOrNickname);
         }
         return new UserAccount(account);
+    }
+
+    public void completeSignUp(Account account) {
+        account.completeSignUp();
+        login(account);
     }
 }
