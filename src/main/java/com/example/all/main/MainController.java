abacfba03d9157bd7dfef53a42c2ef21785ec232
jihoonheo <modernflow@tripstore.kr>
2022-10-10 20:08:13 +0900

    feat: Authority annotation

---
+++ src/main/java/com/example/all/main/MainController.java
@@ -0,0 +1,19 @@
+package com.example.all.main;
+
+import com.example.all.account.CurrentUser;
+import com.example.all.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
