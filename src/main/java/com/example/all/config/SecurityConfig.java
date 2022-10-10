e1b1ca687cb484360f7b9e7216e8bd618a070d1f
moflow <32387358+hojihun5516@users.noreply.github.com>
2022-10-11 06:48:12 +0900

    Revert "chore: Set build npm"
    This reverts commit 91b42704a542fac53297d83bfa2a42cdcc1d6d4d.

--- src/main/java/com/example/all/config/SecurityConfig.java
+++ src/main/java/com/example/all/config/SecurityConfig.java
@@ -16,8 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz.mvcMatchers("/", "/login", "/sign-up", "/check-email",
-                        "/check-email-token", "/email-lgoin", "/check-email-login", "/login-link", "profile/*", "/node_modules/**"
-                ).permitAll()
+                        "/check-email-token", "/email-lgoin", "/check-email-login", "/login-link", "profile/*").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated());
         return http.build();
