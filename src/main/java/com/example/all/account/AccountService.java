6d32b49128a29cd1215bdf93c40c89f900eef871
moflow <32387358+hojihun5516@users.noreply.github.com>
2022-10-11 06:48:12 +0900

    Revert "feat: Set authorization thymleaf navbar"
    This reverts commit 6ee6bc93606a9f93a3e03245ddd149054243ecd9.

--- src/main/java/com/example/all/account/AccountService.java
+++ src/main/java/com/example/all/account/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
