c927ec8cc9b621d3e7a438e17ad7ddd0acc45acc
moflow <32387358+hojihun5516@users.noreply.github.com>
2022-10-11 06:48:12 +0900

    Revert "feat: SignUp mailsender log"
    This reverts commit df1cae24ba6fe52895eccf5251f4111def1be3f3.

--- src/main/java/com/example/all/account/AccountController.java
+++ src/main/java/com/example/all/account/AccountController.java
@@ -1,15 +1,13 @@
 package com.example.all.account;
 
-import com.example.all.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -19,12 +17,10 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
 
-    private final JavaMailSender mailSender;
     //SignUpForm의 camelcase
     @InitBinder("signUpForm")
-    public void initBinder(WebDataBinder webDataBinder) {
+    public void initBinder(WebDataBinder webDataBinder){
         webDataBinder.addValidators(signUpFormValidator);
     }
 
@@ -40,25 +36,6 @@ public class AccountController {
             return "account/sign-up";
         }
         // TODO 회원가입 처리
-        Account account = Account.builder()
-                .email(signUpForm.getEmail())
-                .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) //Encoding 해야함
-                .studyCreatedByWeb(true)
-                .studyEnrollmentResultByWeb(true)
-                .studyUpdatedByWeb(true)
-                .build();
-
-        Account newAccount = accountRepository.save(account);
-
-        newAccount.generateEmailCheckToken();
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setSubject("스터디 회원가입인증");
-        mailMessage.setTo(newAccount.getEmail());
-        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
-                "&email=" + newAccount.getEmail());
-
-        mailSender.send(mailMessage);
         return "redirect:/";
     }
 }
--- src/main/java/com/example/all/account/AccountRepository.java
+++ src/main/java/com/example/all/account/AccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    boolean existsByNickname(String nickname);
+    boolean existsByNickName(String nickname);
 
     boolean existsByEmail(String email);
 }
