675105c4146b6b9a3affc5772d167c6e2b13c916
hojihun5516 <hojihun5516@daum.net>
2022-10-11 06:59:02 +0900

    feat: Create profile form

---
+++ src/main/java/com/example/all/settings/Profile.java
@@ -0,0 +1,25 @@
+package com.example.all.settings;
+
+import com.example.all.domain.Account;
+import lombok.Data;
+
+@Data
+public class Profile {
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    public Profile(Account account) {
+        this.bio = account.getBio();
+        this.url = account.getUrl();
+        this.occupation = account.getOccupation();
+        this.location = account.getLocation();
+
+    }
+
+}
