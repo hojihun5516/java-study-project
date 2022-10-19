57efd34eef3d21a97514a30228c6577c4e8182cb
hojihun5516 <hojihun5516@daum.net>
2022-10-20 07:58:16 +0900

    feat: Add zone with init zone info

---
+++ src/main/java/com/example/all/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package com.example.all.zone;
+
+import com.example.all.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
